db88ef2f8d75d6c3fb4f9743b512355c9e8a7054
Vectt0r <douglas_sd0@outlook.com>
2024-08-30 09:54:51 -0300

    login etc

--- src/app/pages/books/books.module.ts
+++ src/app/pages/books/books.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { BooksPageRoutingModule } from './books-routing.module';
-
 import { BooksPage } from './books.page';
 
 @NgModule({
